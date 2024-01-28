6cd1348ee26151bcc7eba691e6585f7d2e781e30
Dangoo2002 <kennydangote893@gmail.com>
2024-01-28 16:20:56 +0300

    first commit

---
+++ UIC.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void createHotAirBalloon(int size);
+
+int main() {
+    int choice;
+
+    do {
+        cout << " Menu:" << endl;
+        cout << "1. Display Original Art" << endl;
+        cout << "2. Display Hot Air Balloon" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Input your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                
+                  cout << "               ____" << endl;
+                cout << "              /    \\" << endl;
+                cout << "             /      \\" << endl;
+                cout << "             /   U   \\" << endl;
+                cout << "             |   I   |" << endl;
+                cout << "             |   C   |" << endl;
+                cout << "             |       |" << endl;
+                cout << "             \\      /" << endl;
+                cout << "              \\     /" << endl;
+                cout << "               \\   /" << endl;
+                cout << "               =====" << endl;
+                cout << "              \\ || /" << endl;
+                cout << "                ||" << endl;
+                cout << "                ####" << endl;
+                cout << "                ####" << endl;
+                break;
+            case 2:
+                int size;
+                cout << "Enter the size of the hot air balloon (2 or above): ";
+                cin >> size;
+                createHotAirBalloon(size);
+                break;
+            case 3:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 3);
+
+    return 0;
+}
+
+void createHotAirBalloon(int size) {
+    int row = 0;
+
+    
+    while (row < size) {
+        cout << setw(size - row) << setfill(' ') << "";
+        cout << setw(2 * row + 1) << setfill('_') << "" << endl;
+        row++;
+    }
+
+ 
+    int midRows = size * 2;
+    int midCol = size * 2;
+    int midCount = 0;
+
+    while (row < midRows + size) {
+        cout << setw(size - 1) << setfill(' ') << "";
+        cout << "/";
+
+        if (midCount < size) {
+            if (midCount == size / 2) {
+                cout << setw(midCol - 3) << setfill(' ') << "UIC";
+            } else {
+                cout << setw(midCol - 1) << setfill(' ') << "";
+            }
+            midCount++;
+        } else {
+            cout << setw(midCol - 1) << setfill(' ') << "";
+        }
+
+        cout << "\\" << endl;
+        row++;
+    }
+
+    
+    cout << setw(size - 1) << setfill(' ') << "";
+    cout << setw(2 * size + 1) << setfill('=') << "" << endl;
+
+    
+    int basketRow = 0;
+    while (basketRow < size) {
+        cout << setw(size - 1) << setfill(' ') << "";
+        cout << setw(2 * size + 1) << setfill('#') << "" << endl;
+        basketRow++;
+    }
+
+    
+    cout << setw(size - 1) << setfill(' ') << "";
+    cout << "\\" << setw(2 * size - 1) << setfill(' ') << "" << "/" << endl;
+}
